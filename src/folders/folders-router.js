ac54a3253e7777424b6ebb10541268710ea8dd1f
India Rodriguez <carnes.s.ia17@gmail.com>
2020-01-18 10:03:16 -0700

    add folders now working and get folder by id is now working.

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -14,5 +14,45 @@ foldersRouter
             })
             .catch(next)
     })
+    .post(jsonParser, (req, res, next) => {
+        const { foldername } = req.body
+        const newFolder = { foldername }
+
+        if (!foldername) {
+            return res.status(400).json({
+                error: { message: `Missing 'foldername' in request body` }
+            })
+        }
+
+        FoldersService.insertFolder(
+            req.app.get('db'),
+            newFolder
+        )
+            .then(folder => {
+                res
+                    .status(201)
+                    .location(`/api/folders/${folder.id}`)
+                    .json(folder)
+            })
+            .catch(next)
+    })
+
+
+foldersRouter
+    .route('/:folder_id')
+    .get((req, res, next) => {
+        const knexInstance = req.app.get('db')
+
+        FoldersService.getById(knexInstance, req.params.folder_id)
+            .then(folder => {
+                if (!folder) {
+                    return res.status(404).json({
+                        error: { message: `Folder doesn't exist` }
+                    })
+                }
+                res.json(folder)
+            })
+            .catch(next)
+    })
 
 module.exports = foldersRouter
--- test/folders-endpoints.spec.js
+++ test/folders-endpoints.spec.js
@@ -46,4 +46,65 @@ describe(`Folders Endpoints`, function () {
             })
         })
     })
+
+    describe('GET /api/folders/:folder_id', () => {
+
+        context(`Given no folders`, () => {
+            it(`Responds with 404`, () => {
+                const folderId = 123456
+                return supertest(app)
+                    .get(`/api/folders/${folderId}`)
+                    .expect(404, { error: { message: `Folder doesn't exist` } });
+            })
+        })
+
+        context(`Given there are folders in the database`, () => {
+            const testFolders = makeFoldersArray()
+
+            beforeEach(() => {
+                return db
+                    .into('folders')
+                    .insert(testFolders)
+            })
+
+            it(`responds with 200 and the specified folder`, () => {
+                const folderId = 3
+                const expectedFolder = testFolders[folderId - 1]
+
+                return supertest(app)
+                    .get(`/api/folders/${folderId}`)
+                    .expect(200, expectedFolder)
+            })
+        })
+    })
+
+    describe('POST /api/folders', () => {
+        it(`creates a folder, responding with 201 and the new folder`, () => {
+            const newFolder = {
+                foldername: 'Test new folder',
+            }
+            return supertest(app)
+                .post('/api/folders')
+                .send(newFolder)
+                .expect(201)
+                .expect(res => {
+                    expect(res.body.foldername).to.eql(newFolder.foldername)
+                    expect(res.body).to.have.property('id')
+                })
+                .then(res =>
+                    supertest(app)
+                        .get(`/api/folders/${res.body.id}`)
+                        .expect(res.body)
+                )
+        })
+
+        it(`responds with 400 and an error message when the 'foldername' is missing`, () => {
+            return supertest(app)
+                .post('/api/folders')
+                .send({})
+                .expect(400, {
+                    error: { message: `Missing 'foldername' in request body` }
+                })
+        })
+    })
 })
